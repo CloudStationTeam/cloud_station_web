a969092c00291db4d76e72f8bd81bbaf184c6c87
minabedwany <mina.bedwany@gmail.com>
2019-11-16 18:24:46 -0800

    used our own login page and removed crispy forms dependancy

--- webgms/settings.py
+++ webgms/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
 ]
 
 CRISPY_TEMPLATE_PACK =  'bootstrap4'
