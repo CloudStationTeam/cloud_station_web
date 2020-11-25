67549b9cc3d1042ed0f5d871a8633ef90268f6e4
Ubuntu <ubuntu@ip-172-31-37-157.ec2.internal>
2020-11-25 20:00:36 +0000

    Created basic UI for Telemetry customization

--- flightmonitor/static/js/common.js
+++ flightmonitor/static/js/common.js
@@ -283,3 +283,34 @@ function set_mode_test(id, value) {
     alert(id + value);
     return false;
 }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("telemetryEditBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function submitTelemetry(){
+	var x = document.getElementById("timeBootCheck").checked;
+	document.getElementById("submitTelemetryBtn").innerHTML = x;
+
+}
+
