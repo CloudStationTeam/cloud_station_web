1f75d0f434910291211cfb5eb37a91ccbb7cf7c7
Chris Zhang <chizhang95@gmail.com>
2020-12-09 11:58:07 -0800

    added SYS_STATUS

--- flight_data_collect/drone_communication/mavlink_constants.py
+++ flight_data_collect/drone_communication/mavlink_constants.py
@@ -80,5 +80,8 @@ AVAILABLE_TELEMETRY_DATA = {
     "GLOBAL_POSITION_INT": ["time_boot_ms", "lat", "lon", "alt", "relative_alt", "vx", "vy", "vz", "hdg"],
     "BATTERY_STATUS": ["id", "battery_function", "type", "temperature", "voltages", "current_battery",
                        "current_consumed", "energy_consumed", "battery_remaining", "time_remaining", "charge_state",
-                       "voltages_ext", "mode", "fault_bitmask"]
+                       "voltages_ext", "mode", "fault_bitmask"],
+    "SYS_STATUS": ["onboard_control_sensors_present", "onboard_control_sensors_enabled",
+                   "onboard_control_sensors_health", "load", "voltage_battery", "current_battery", "battery_remaining",
+                   "drop_rate_comm", "errors_comm", "errors_count1", "errors_count2", "errors_count3", "errors_count4"]
 }
