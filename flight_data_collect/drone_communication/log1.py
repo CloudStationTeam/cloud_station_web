98be709f7e9c79c0efac2842d020fa74944c418f
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-06-27 11:07:13 -0700

    Update log1.py

--- flight_data_collect/drone_communication/log1.py
+++ flight_data_collect/drone_communication/log1.py
@@ -2,11 +2,12 @@ import logging
 
 def log11():
   # Configure the logging module to display the name of the logger, the level of the log message, and the message.
-  logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+  #logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
   
   # Get a logger with the name 'myLogger'. Multiple calls to `getLogger('myLogger')` will always return a reference to the same Logger object.
-  logger = logging.getLogger('myLogger')
-
+  #logger = logging.getLogger('myLogger')
+  logger = logging.getLogger(__name__)
+  
   # Log messages with various importance levels.
   logger.debug('This is a debug message')  # Not shown because the level is set to INFO
   logger.info('This is an info message')
