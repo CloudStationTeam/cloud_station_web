9e9431208e0516322d75c6516540bd87a2ed6c56
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-01 17:08:50 -0700

    Update lidar_data.py

--- flight_data_collect/drone_communication/lidar_data.py
+++ flight_data_collect/drone_communication/lidar_data.py
@@ -1,5 +1,43 @@
 from pymavlink import mavutil
 
+def find_vacant_channel(master):
+    # Retrieve RC_CHANNELS message
+    msg = master.recv_match(type='RC_CHANNELS', blocking=True)
+
+    # Iterate through RC channel values to find vacant channels
+    vacant_channels = []
+    for i in range(1, msg.chancount + 1):
+        # Check if the channel value is unassigned or unused
+        if msg['chan{}_raw'.format(i)] == 0:
+            vacant_channels.append(i)
+            return i
+
+    #print("Vacant channels: ", vacant_channels)
+
+    #if not len(vacant_channels):
+    return -1
+
+def find_switch_threshold(master, channel_number):
+    # Set the RCx_OPTION parameter to 40 (replace x with the vacant channel number)
+    rc_option_param = "RC{}_OPTION".format(channel_number)
+    master.param_set(rc_option_param, 40)
+
+    # Monitor the state of the switch and find the threshold value
+    threshold_value = None
+
+    print(f"Toggle the switch associated with channel {channel_number} to the HIGH position.")
+    while threshold_value is None:
+        msg = master.recv_match(type='RC_CHANNELS', blocking=True)
+        threshold_value = msg.getattr(f"chan{channel_number}_raw")
+    print(f"Threshold value for HIGH position: {threshold_value}")
+
+    print(f"Toggle the switch associated with channel {channel_number} to the LOW position.")
+    while threshold_value is not None:
+        msg = master.recv_match(type='RC_CHANNELS', blocking=True)
+        if msg.getattr(f"chan{channel_number}_raw") < threshold_value:
+            threshold_value = None
+    print(f"Threshold value for LOW position: {threshold_value}")
+
 def main1():
     # Connect to the autopilot
     master = mavutil.mavlink_connection('udpout:localhost:14550')
@@ -39,4 +77,11 @@ def main1():
         param_value=1,
         param_type=mavutil.mavlink.MAV_PARAM_TYPE_UINT8
     )
+
+    n = find_vacant_channel(master)
+    
+    # Set RCx_OPTION to 40
+    if n != -1:
+        find_switch_threshold(1master, n)
+
 
