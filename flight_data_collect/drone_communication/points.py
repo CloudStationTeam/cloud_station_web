6ae0accde05fcc092decfff6587fa223bd400a77
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-02 18:36:58 -0700

    Update points.py

--- flight_data_collect/drone_communication/points.py
+++ flight_data_collect/drone_communication/points.py
@@ -1,3 +1,5 @@
+from pymavlink import mavutil
+
 import requests
 
 def get_lat_lon_alt(address):
@@ -35,4 +37,28 @@ ex.
 lat, lon, alt = get_lat_lon_alt("1600 Amphitheatre Parkway, Mountain View, CA")
 print(f"Latitude: {lat}, Longitude: {lon}, Altitude: {alt}")
 '''
+
+def send_waypoint(connection, lat, lon, alt, seq):
+    """
+    Send a waypoint to the drone.
+
+    For the altitude obtained from the Open Topo Data API,
+    you should use MAV_FRAME_GLOBAL for your MAVLink commands.
+    This frame uses the WGS84 coordinate system where altitude is relative to mean sea level.
+    This is appropriate because the altitude obtained from the Open Topo Data API is also relative to mean sea level
+    """
+    connection.mav.mission_item_send(
+        connection.target_system,
+        connection.target_component,
+        seq, # Sequence number of the waypoint
+        mavutil.mavlink.MAV_FRAME_GLOBAL, # The frame of reference
+        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, # The command, in this case, to navigate to a waypoint
+        0, # Current waypoint
+        0, # Autocontinue to next waypoint
+        0, 0, 0, 0, # Parameters 1-4 not used
+        lat, lon, alt) # Parameters 5-7 are latitude, longitude, altitude
+
+def test(the_connection):
+    lat, lon, alt = get_lat_lon_alt("1600 Pennsylvania Ave, Washington, DC")
+    send_waypoint(the_connection, lat, lon, alt, seq=0)
 
