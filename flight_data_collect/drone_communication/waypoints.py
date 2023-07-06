88b1fa1d3b6fb21f3e5394558a6578d283f3c44b
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-05 20:09:43 -0700

    Update waypoints.py

--- flight_data_collect/drone_communication/waypoints.py
+++ flight_data_collect/drone_communication/waypoints.py
@@ -268,7 +268,7 @@ def main1(addrList=None): #done.
   #"""
   n = 0
   for addr in addrList:
-    lat, lon, alt = points.get_lat_lon_alt(addrList)
+    lat, lon, alt = points.get_lat_lon_alt(addr)
     mission_waypoints.append(mission_item(n, 0, lat, lon, alt))
     n += 1
     
