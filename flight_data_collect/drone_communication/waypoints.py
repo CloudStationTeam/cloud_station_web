776354bc1d13d7a4d868b98da0f161f73a91574b
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-08 10:55:54 -0700

    Update waypoints.py

--- flight_data_collect/drone_communication/waypoints.py
+++ flight_data_collect/drone_communication/waypoints.py
@@ -55,6 +55,13 @@ def arm(the_connection): #done.
 mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0,0,0,0,0,0) # 0 conf, 1 arm, else irrelevant.
     
     ack(the_connection, "COMMAND_ACK")
+
+    n = 1
+    #while True:
+    while not mavlink.motors_armed() and n<10:
+      time.sleep(1)
+      print("wait arm"+str(n))
+      n+=1
         
 #Takeoff the Drone
 def takeoff(the_connection): #done.
