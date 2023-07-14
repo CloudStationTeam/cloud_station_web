bece3d64c73f75b9399e09fd72ccd1f9d25e356e
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-14 15:42:01 -0700

    Update mavlink_control.py

--- flight_data_collect/drone_communication/mavlink_control.py
+++ flight_data_collect/drone_communication/mavlink_control.py
@@ -5,10 +5,7 @@ from flight_data_collect.drone_communication.mavlink_constants import MAVLINK_MS
 from flightmonitor.consumers import send_message_to_clients
 import time
 
-#import flight_data_collect.drone_communication.smt
-from . import waypoints
-from . import log1
-from . import lidar_data
+from . import waypoints, lidar_data
 
 SERVER_IP = socket.gethostbyname(socket.gethostname())
 
@@ -182,34 +179,18 @@ def fly_to_point(connect_address: int, lat, lon, alt):
         print(e)
         return {'ERROR': str(e), 'droneid': connect_address}
 
-#addrList=[] #static 
+#addrList=[] #sql
 def update_waypoints(connect_address: int, addr: str):
     print("wp") #reached.
     try:
-        #return "???    Hello? " + "str(SERVER_IP)" + ':' + str(connect_address)
-        #mavlink = mavutil.mavlink_connection(SERVER_IP + ':' + str(connect_address))
-        msg = log1.log11() #done.
-        #return msg
-        
-        print("smt")
-        """
-        global addrList
-        addrList.append(address)
-        if len(addrList) < 3:
-            return 
-        print("addrList "+addrList)
-        """
         addrList = [addr]
         msg = waypoints.main1(addrList) #just debug line by line.
-        #addrList = []
         if not msg:
             msg = "None"
         print(msg)
         return "// // // // " + str(msg)
     except Exception as e:
         print(e)
-        stre = str(e)
-        return str({'// // // // ERROR': 'Arm/Disarm command failed!' + stre, 'droneid': connect_address})
+        return str({'// // // // ERROR': str(e), 'droneid': connect_address})
 
     
-
