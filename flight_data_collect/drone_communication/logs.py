1f92c12facfb845c95a2c5c58228b2411e5eb008
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-14 16:04:08 -0700

    Update logs.py

--- flight_data_collect/drone_communication/logs.py
+++ flight_data_collect/drone_communication/logs.py
@@ -6,12 +6,4 @@ def log(s):
   logger = logging.getLogger(__name__)
   
   logger.info(s)
-  
-"""
-reload X
-manage.py X
-log1.py X
-commons.js X
-con.py not the last one
-views.py not the last one X
-"""
+
