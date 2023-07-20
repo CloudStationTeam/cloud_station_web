4c7ed7436b458a939eb24cfc744471a999a6534c
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-20 15:19:20 -0700

    Update waypoints.py

--- flight_data_collect/drone_communication/waypoints.py
+++ flight_data_collect/drone_communication/waypoints.py
@@ -195,7 +195,7 @@ def add(addrList=None): #done.
   """
   #"""
   for addr in addrList:
-    lat, lon, alt = points.get_lat_lon_alt(addr[3:])
+    #lat, lon, alt = points.get_lat_lon_alt(addr[3:])
     #free map api timed out. #No.
     #lat, lon, alt = tups[n-1]
     alt = 5
