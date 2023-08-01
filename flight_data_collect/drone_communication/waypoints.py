88d694e319565621d78461747582fa2c0acbd7cb
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-07-31 20:56:09 -0700

    Update waypoints.py

--- flight_data_collect/drone_communication/waypoints.py
+++ flight_data_collect/drone_communication/waypoints.py
@@ -195,10 +195,15 @@ def add(addrList=None): #done.
   """
   #"""
   for addr in addrList:
+    """
     #lat, lon, alt = points.get_lat_lon_alt(addr[3:])
     #free map api timed out. #No.
     lat, lon, alt = tups[n-1]
     alt = 5
+    """
+    lat, lon = points.get_lat_lon(addr[3:])
+    alt = 5
+    
     print("Now. ", lat, lon, alt)
     mission_waypoints.append(mission_item(n, 0, float(lat), float(lon), int(alt)))
     n += 1
