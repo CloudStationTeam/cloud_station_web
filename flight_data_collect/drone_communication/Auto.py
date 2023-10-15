db946e26b84e2d2c4623cfd2737f29f30b22dc47
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-10-14 23:48:13 -0700

    Update Auto.py

--- flight_data_collect/drone_communication/Auto.py
+++ flight_data_collect/drone_communication/Auto.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import requests
 
 def autocomplete_view(query):
-    response = requests.get(f'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={query}&key={settings.API_KEY}')
+    response = requests.get(f'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={query}&key={settings.Google_Map_API_KEY}')
     return JsonResponse(response.json())
 
 def test_autocomplete_view():
