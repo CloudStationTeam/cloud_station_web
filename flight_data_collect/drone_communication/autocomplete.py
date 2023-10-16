5c83474168630073e5466efd9da06ca189accac5
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-10-16 03:22:23 -0700

    Update autocomplete.py

--- flight_data_collect/drone_communication/autocomplete.py
+++ flight_data_collect/drone_communication/autocomplete.py
@@ -2,8 +2,17 @@ import requests
 from django.conf import settings
 from django.http import JsonResponse
 
-def autocomplete_view(query):
-    response = requests.get(f'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={query}&key={settings.GOOGLE_MAP_API_KEY}')
+def autocomplete_view(query, location=None):
+    params = {
+        'input': query,
+        'key': settings.GOOGLE_MAP_API_KEY,
+    }
+    if location:
+        params['location'] = location  # This biases the autocomplete predictions to the specified location
+
+    response = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json', params=params)
+
+    #response = requests.get(f'https://maps.googleapis.com/maps/api/place/autocomplete/json?input={query}&key={settings.GOOGLE_MAP_API_KEY}')
     print(response.text if response.status_code == 200 else None) # Not all eq, Not pass 
     #return JsonResponse(response.json()) # TypeError: Object of type 'JsonResponse' is not JSON serializable
     return response.json()
