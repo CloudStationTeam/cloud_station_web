0aa5d4e812abd877bc99bd3833c7f2c22ad65a1c
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-10-16 03:04:32 -0700

    Update sanitize_text.py

--- flight_data_collect/drone_communication/sanitize_text.py
+++ flight_data_collect/drone_communication/sanitize_text.py
@@ -3,6 +3,6 @@ import re
 def sanitize_text(text_input):
     return re.sub(r'[^a-zA-Z0-9,. ]', '', text_input)
 
-sanitized_text = sanitize_text(user_input)
-
+def test(user_input):
+    sanitized_text = sanitize_text(user_input)
 
