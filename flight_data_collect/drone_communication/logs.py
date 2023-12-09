d88f33b0bcc673ae089dfec2d745f17a40f966a2
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-12-09 06:36:54 -0800

    Update logs.py

--- flight_data_collect/drone_communication/logs.py
+++ flight_data_collect/drone_communication/logs.py
@@ -5,7 +5,7 @@ def log(*args, **kwargs): # logs.log(1, 2, 3, a=4, b=5)
   s = ""
   for item in args:
     s += str(item) + ", " 
-  for item in d.items():
+  for item in kwargs.items():
     s += str(item) + ", " 
 
   logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
