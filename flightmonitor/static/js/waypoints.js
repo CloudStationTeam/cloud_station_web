3b6db318feca31a652f6c5f4e1fe030a052d0c99
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-12-09 17:29:09 -0800

    Update waypoints.js

--- flightmonitor/static/js/waypoints.js
+++ flightmonitor/static/js/waypoints.js
@@ -124,38 +124,11 @@ getClientLocation()
     });
 */
 
-/*
-// Function to retrieve suggested addresses from the Nominatim API
-// Google Map API is probably better but it costs money.
-async function getSuggestedAddresses(query) {
-  //const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=json&limit=5`);
-//let points = await getPoints();
-//let lat = points[0]
-//let lon = points[1]
 
-let lat = 33.6431;
-let lon = -117.8419;
-  
-let url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(query)}&format=json&limit=5&lat=${lat}&lon=${lon}`;
-
-  //var url = `https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${encodeURIComponent(query)}&format=json`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-  }
-
-}
-*/
 
 
 addressInput.addEventListener("input", async () => {
   const value = filterit(addressInput.value.trim());
-  //alert(value);
   console.log(value);
   
   // Clear the previous suggestions
@@ -165,41 +138,22 @@ addressInput.addEventListener("input", async () => {
     return;
   }
   
-  // Get the suggested addresses from the Nominatim API
-  //const suggestedAddresses = await getSuggestedAddresses(value);
-  
-  // Call the function with a query
-  //const suggestedAddresses = getAutocompleteResults('1 Shields Ave., Davis, CA 95616');
+  // Call the function with a query 
   let suggestedAddresses = null;
   try {
-        //alert("?")
         let data = await autocomplete(String(value)); //a proms of json
-        //alert(response);
-        /*
-        if (!response.ok) {
-            alert("???")
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        let data = await response.json();
-        */
-        //alert(data)
-        //alert(2)
-        //alert(data.predictions)
-        //alert(data);
         data = JSON.parse(data) // Ref: https://www.w3schools.com/js/js_json_objects.asp
         //alert(data);
         suggestedAddresses = data.predictions; 
-        //alert(suggestedAddresses);
     } catch (error) {
         console.error(error);
   }
-  //alert(suggestedAddresses[1].description)
-  //alert(1)
   
   // Create suggestion items and append them to the container
   suggestedAddresses.forEach(item => {
     const suggestion = document.createElement("div");
     suggestion.classList.add("suggested-address");
+    //alert(item);
     suggestion.textContent = item.description;
     suggestion.addEventListener("click", () => {
       addressInput.value = item.description;
@@ -239,8 +193,6 @@ send_droneid.addEventListener("click", () => {
   //clear
   clearAllButton.click();
 })
-
-
   
 
 //2. Show wps 
@@ -291,6 +243,7 @@ return;
   showAllWaypoints();
 });
 
+
 clearAllButton.addEventListener("click", () => {
   // Clear all waypoint lists and the display
   let droneid = filterit(droneid2.value);
@@ -304,8 +257,8 @@ clearAllButton.addEventListener("click", () => {
   showAllWaypoints();
 });
 
+
 function renderWaypointList(address) {
-  //alert(address);
   // Create a new <div> element for the address
   const divElement = document.createElement("div");
 
