6dbd24c16da9bf650abee854251af754d798dab1
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-12-10 11:30:13 -0800

    Update mavlink_control.py

--- flight_data_collect/drone_communication/mavlink_control.py
+++ flight_data_collect/drone_communication/mavlink_control.py
@@ -118,7 +118,7 @@ def set_arm(connect_address: int, is_disarm=False):
 
 def fly_to_point(connect_address: int, lat, lon, alt):
     try:
-        add = ",".join(list(map(str, [lat,lon,alt])))
+        addr = ",".join(list(map(str, [lat,lon,alt])))
         logs.log(addr) #"1,2,3"
         addrList = [addr, addr] #["1,2,3", "1,2,3"]
         msg = waypoints.add(connect_address, addrList) 
