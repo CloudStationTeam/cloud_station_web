81ab436231df06899a2e1ddf319fe0726d6dcf3e
Peter Burke <42294224+PeterJBurke@users.noreply.github.com>
2023-12-28 16:30:26 -0800

    Update urls.py
    cleaned mis pasted code example

--- flight_data_collect/urls.py
+++ flight_data_collect/urls.py
@@ -3,13 +3,6 @@
 
 from django.urls import include, re_path
 
-#from myapp.views import home
-
-urlpatterns = [
-    re_path(r'^$', home, name='home'),
-    re_path(r'^myapp/', include('myapp.urls'),
-]
-
 
 #from django.urls import path
 from . import views
