8067e18b95ba421fd8ed77c68cf16dcd5f3894c2
Peter Burke <42294224+PeterJBurke@users.noreply.github.com>
2023-12-28 16:17:50 -0800

    Update routing.py
    updated to Django 5

--- webgms/routing.py
+++ webgms/routing.py
@@ -3,13 +3,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from flightmonitor.consumers import UserActionsConsumer
-from django.conf.urls import url
+
+from django.urls import include, re_path
+
+#from django.conf.urls import url # not available in Django 5
+# see https://stackoverflow.com/questions/70319606/importerror-cannot-import-name-url-from-django-conf-urls-after-upgrading-to
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter([
-            url(r'ws/flightmonitor/$', UserActionsConsumer)
+            re_path(r'ws/flightmonitor/$', UserActionsConsumer)
         ])
     ),
 })
