5f822a773ad4c228252b154535a4e7ac137a2cf9
Peter Burke <42294224+PeterJBurke@users.noreply.github.com>
2023-12-28 19:54:15 -0800

    Update apps.py
    fixed capitalization of name of flightmonitor app

--- flightmonitor/apps.py
+++ flightmonitor/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class FlightmonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Flightmonitor'
+    name = 'flightmonitor'
