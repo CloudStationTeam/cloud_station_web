1b711b44465f84ada16010b9ebb690867616a375
Peter Burke <42294224+PeterJBurke@users.noreply.github.com>
2023-12-28 20:19:14 -0800

    Update urls.py
    path to repath, the old version for the old way of formatting urls and paths

--- flight_data_collect/urls.py
+++ flight_data_collect/urls.py
@@ -6,15 +6,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^connect/(?P<connect_address>[^/]+)/$', views.connect_vehicle, name='connect-vehicle'),
-    path(r'disconnect/(?P<connect_address>[^/]+)/$', views.disconnect_vehicle, name='disconnect-vehicle'),
-    path(r'control/setmode/(?P<droneid>[^/]+)/(?P<mode>[^/]+)/$', views.set_mode, name='set-mode'),
-    path(r'control/setwaypoint/(?P<droneid>[^/]+)/(?P<lat>[^/]+)/(?P<lon>[^/]+)/(?P<alt>[^/]+)/$', views.set_waypoint,
+    re_path(r'^connect/(?P<connect_address>[^/]+)/$', views.connect_vehicle, name='connect-vehicle'),
+    re_path(r'disconnect/(?P<connect_address>[^/]+)/$', views.disconnect_vehicle, name='disconnect-vehicle'),
+    re_path(r'control/setmode/(?P<droneid>[^/]+)/(?P<mode>[^/]+)/$', views.set_mode, name='set-mode'),
+    re_path(r'control/setwaypoint/(?P<droneid>[^/]+)/(?P<lat>[^/]+)/(?P<lon>[^/]+)/(?P<alt>[^/]+)/$', views.set_waypoint,
         name='set-waypoint'),
-    path(r'control/arm/(?P<droneid>[^/]+)/$', views.arm, name='arm'),
-    path(r'control/disarm/(?P<droneid>[^/]+)/$', views.disarm, name='arm'),
-    path(r'control/flyto/(?P<droneid>[^/]+)/(?P<lat>[^/]+)/(?P<lon>[^/]+)/(?P<alt>[^/]+)/$', views.fly_to,
+    re_path(r'control/arm/(?P<droneid>[^/]+)/$', views.arm, name='arm'),
+    re_path(r'control/disarm/(?P<droneid>[^/]+)/$', views.disarm, name='arm'),
+    re_path(r'control/flyto/(?P<droneid>[^/]+)/(?P<lat>[^/]+)/(?P<lon>[^/]+)/(?P<alt>[^/]+)/$', views.fly_to,
         name='fly-to'),
-    path(r'update-fields/$', views.update_fields, name='update-fields'),
-    path(r'get-available-fields/$', views.get_available_fields, name='get-available-fields')
+    re_path(r'update-fields/$', views.update_fields, name='update-fields'),
+    re_path(r'get-available-fields/$', views.get_available_fields, name='get-available-fields')
 ]
