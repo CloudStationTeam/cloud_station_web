fa4c23d4f49307615feb9e6122d83dadefb50e99
Peter Burke <42294224+PeterJBurke@users.noreply.github.com>
2023-12-28 19:00:33 -0800

    Update asgi.py
    updated to template from Django 5

--- webgms/asgi.py
+++ webgms/asgi.py
@@ -1,12 +1,17 @@
+
 """
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgms.settings")
-django.setup()
-application = get_default_application()
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webgms.settings')
+
+application = get_asgi_application()
