5edc78bd4b463525a61d6fc15e7b1511e7e6d77f
Peter Burke <peter.burke.john@gmail.com>
2023-12-31 12:53:38 -0800

    revert to version 2.0 of asgi, it broke websockets before

--- webgms/asgi.py
+++ webgms/asgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webgms.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgms.settings")
+django.setup()
+application = get_default_application()
