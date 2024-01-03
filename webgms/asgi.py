1130049a90cada29054732f106ae6d2ee61e086c
Peter Burke <peter.burke.john@gmail.com>
2024-01-03 10:03:30 -0800

    change asgi.py as per Django channels tutorial

--- webgms/asgi.py
+++ webgms/asgi.py
@@ -7,12 +7,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
-import os
-import django
-from channels.routing import get_default_application
-
+#import os
+#import django
+#from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgms.settings")
-django.setup()
-application = get_default_application()
+
+#django.setup()
+#application = get_default_application()
+#from django.core.asgi import get_asgi_application
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
