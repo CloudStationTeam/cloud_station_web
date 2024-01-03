6b64ccc09aa1737a05ed43888d2510b4e9d62bd0
Peter Burke <peter.burke.john@gmail.com>
2024-01-03 11:31:21 -0800

    changed to match tutorial (no routing.py in webgms, new routing.py in flightmonitor, asgi.py has router based on protocol

--- flightmonitor/routing.py
+++ flightmonitor/routing.py
@@ -1,1 +1,9 @@
 # flightmonitor/routing.py
+
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/flightmonitor/$', consumers.UserActionsConsumer.as_asgi()),
+]
+
--- webgms/asgi.py
+++ webgms/asgi.py
@@ -7,21 +7,30 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-#import os
+import os
 #import django
 #from channels.routing import get_default_application
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgms.settings")
+from flightmonitor.routing import websocket_urlpatterns
 
-#django.setup()
-#application = get_default_application()
-#from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgms.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import flightmonitor.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
