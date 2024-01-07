6b204ff2d12044dadc8aa80196a5cf8a8c0483a2
Peter Burke <peter.burke.john@gmail.com>
2024-01-06 21:41:32 -0800

    connect disconnect connect disconnect works now

--- flightmonitor/listen.py
+++ flightmonitor/listen.py
@@ -36,16 +36,20 @@ def listenfunction(droneid_to_listen_to, mavlinkconnection, websocket_to_send_to
     vehicle_to_listen_to = Vehicle.objects.get(droneid=droneid_to_listen_to)
 
     while vehicle_to_listen_to.is_connected: # now get all messages and log to terminal
+    
 
         msg = mavlinkconnection.recv_match( blocking=True)
         #msg = mavlink.recv_match(type='GPS_RAW_INT', blocking=True)
         #handle_mavlink_message_to_update_Django_drone_object(msg, connect_address)
         message_type = msg.get_type() # parse message type
-        vehicle_to_listen_to.refresh_from_db()
-        print(' vehicle_to_listen_to.is_connected= ',droneid_to_listen_to,vehicle_to_listen_to.is_connected,vehicle_to_listen_to)
 
 
         if(message_type in USEFUL_MESSAGES_V4_0_PYTHON):
             #print('[LOG][consumers.py] received message_type in USEFUL_MESSAGES_V4_0_PYTHON at time ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             #print('msg = ',msg)
             websocket_to_send_to.send(msg.to_json()) # Send MAVLink message as a JSON string to the WebSocket client
+        vehicle_to_listen_to.refresh_from_db()
+        print(' vehicle_to_listen_to.is_connected= ',droneid_to_listen_to,vehicle_to_listen_to.is_connected,vehicle_to_listen_to)
+        # if it is disconnect now, close mavlink
+        if(vehicle_to_listen_to.is_connected==False):
+            mavlinkconnection.close()
